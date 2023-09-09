d221dc89dc6f5b0a2d27710b5d181e03c8e69be9
Licar <84367661+Licarsmeth@users.noreply.github.com>
2023-09-09 18:28:45 +0545

    html and css done. js to add books done.

---
+++ script.js
@@ -0,0 +1,103 @@
+const myLibrary = [];
+
+function Book(author, title, page, read) {
+  this.author = author;
+  this.title = title;
+  this.page = page;
+  this.read = read;
+}
+
+const harry_potter = new Book("JK Rowling", "Harry Potter", 400, true);
+const wrath = new Book("Wrath", "John Gwynne", 350, false);
+
+function addBookToLibrary(name) {
+  myLibrary.push(name);
+}
+addBookToLibrary(harry_potter);
+addBookToLibrary(wrath);
+console.log(myLibrary);
+
+function addRow(book) {
+  // Get a reference to the table body
+  const tableBody = document.getElementById("table-body");
+
+  // Create a new table row
+  const newRow = document.createElement("tr");
+
+  // Create and add table data cells (td) for each column
+  const titleCell = document.createElement("td");
+  titleCell.textContent = book.title;
+  const authorCell = document.createElement("td");
+  authorCell.textContent = book.author;
+  const pageCell = document.createElement("td");
+  pageCell.textContent = book.page;
+  const readCell = document.createElement("td");
+  readCell.textContent = book.read ? "👍" : "👎";
+
+  //create delete button
+  const delCell = document.createElement("button");
+  delCell.classList.add("delCell");
+  delCell.textContent = "X";
+
+  // Append the cells to the row
+  newRow.appendChild(titleCell);
+  newRow.appendChild(authorCell);
+  newRow.appendChild(pageCell);
+  newRow.appendChild(readCell);
+  newRow.appendChild(delCell);
+
+  // Append the row to the table body
+  tableBody.appendChild(newRow);
+}
+
+addRow(wrath);
+addRow(harry_potter);
+
+// Get the modal and button elements
+const dialog = document.getElementById("bookModal");
+const openModalBtn = document.querySelector("#openModalBtn button");
+const closeModalBtn = document.getElementById("closeModalBtn");
+const saveBookBtn = document.getElementById("saveBookBtn");
+
+// Event listener to open the modal
+openModalBtn.addEventListener("click", () => {
+  //reset all values
+  bookName.value = "";
+  author.value = "";
+  page.value = "";
+  read.checked = "false";//somehow this doesn't work
+
+  dialog.showModal();
+});
+
+// Event listener to close the modal when clicking the close button
+closeModalBtn.addEventListener("click", () => {
+  dialog.close();
+});
+
+// Event listener to close the modal when clicking outside the modal content
+window.addEventListener("click", (event) => {
+  if (event.target === dialog) {
+    dialog.close();
+  }
+});
+
+// Event listener to handle saving the book information
+saveBookBtn.addEventListener("click", () => {
+  const bookName = document.getElementById("bookName").value;
+  const author = document.getElementById("author").value;
+  const page = document.getElementById("page").value;
+  const read = document.getElementById("read").checked;
+
+  // Handle the book name and author as needed
+  let tempBookName = bookName.toLowerCase().replace(/ /g, "_");
+  tempBookName = new Book(author, bookName, page, read);
+  addBookToLibrary(tempBookName);
+
+  //add the added book to the row
+  addRow(myLibrary[myLibrary.length - 1]);
+  // Close the modal
+  dialog.close();
+});
+
+//toggle read
